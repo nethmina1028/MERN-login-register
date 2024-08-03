29744b11c3832ff584c3eb7a960a1c154dad6236
Wickramasinghe W A N P <139307869+nethmina1028@users.noreply.github.com>
2024-08-03 22:56:49 +0530

    fronteend login and register

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { useState } from 'react'
 
 function Login() {
+      
+  const [data,setData] = useState({
+    email:'',
+    password:'',
+  })
+      const loginUser =  (e) => {
+         e.preventDefault()
+      }
   return (
-    <div>Login</div>
+    <div> 
+      <form onSubmit={loginUser}>
+      <label>Email</label>
+          <input type='email' placeholder='enter email...' value={data.email} onChange={(e) =>setData({...data, email:e.target.value})} />
+
+          <label>Password</label>
+          <input type='password' placeholder='enter password...'value={data.password} onChange={(e) =>setData({...data, password:e.target.value})}  />
+
+          <button type='submit'>Login</button>
+      </form>
+    </div>
   )
 }
 
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { useState } from 'react'
 
 function Register() {
+
+    const [data,setData] = useState({
+        name:'',
+        email:'',
+        password:'',
+    })
+           
+        const registerUser =  (e) => {
+        e.preventDefault()
+         }
   return (
-    <div>Register</div>
+  <div>
+    <form onSubmit={registerUser}>
+         <label>Name</label>
+          <input type='text' placeholder='enter name...' value={data.name} onChange={(e) =>setData({...data, name:e.target.value})} />
+
+          <label>Email</label>
+          <input type='email' placeholder='enter email...'value={data.email} onChange={(e) =>setData({...data, email:e.target.value})}  />
+
+          <label>Password</label>
+          <input type='password' placeholder='enter password...'value={data.password} onChange={(e) =>setData({...data, password:e.target.value})}  />
+          
+          <button type='submit'>Submit</button>
+    </form>
+  </div>
   )
 }
 
